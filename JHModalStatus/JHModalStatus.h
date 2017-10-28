658d572897dea4cfeed0f23e613277994ba95d2f
Hallac Juliano <juliano.hallac@gmail.com>
2017-10-28 13:03:04 -0200

    Initial Commit

---
+++ JHModalStatus/JHModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  JHModalStatus.h
+//  JHModalStatus
+//
+//  Created by Juliano Hallac on 28/10/17.
+//  Copyright © 2017 Juliano Hallac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHModalStatus.
+FOUNDATION_EXPORT double JHModalStatusVersionNumber;
+
+//! Project version string for JHModalStatus.
+FOUNDATION_EXPORT const unsigned char JHModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHModalStatus/PublicHeader.h>
+
+
